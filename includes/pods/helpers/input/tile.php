e9672c1f27149eb8dfb2aff894f9e381cadf2703
andrea rota <a.rota@lse.ac.uk>
2013-09-19 12:22:29 +0100

    updated helper for Podsv2

--- includes/pods/helpers/input/tile.php
+++ includes/pods/helpers/input/tile.php
@@ -6,6 +6,7 @@
  */
 
 $PODS_BASEURI_NEW_ITEM = '/wp-admin/admin.php?page=';
+$active_tile_id = $value[0];
 ?>
 <script>
 // let ddslick dropdowns at bottom of page display in full
@@ -15,21 +16,20 @@ jQuery(function($) { $('.pods_form').css({overflow: 'visible'}); });
 <div id="<?php echo $css_id; ?>_container">
 <select name="<?php echo $name; ?>" id="pods_form1_<?php echo $name; ?>" class="form pick1 <?php echo $name; ?> pods_field pods_field_<?php echo $name; ?> pods_coltype_pick">
   <option value="">-- Select one --</option>
-  <?php if ( !empty( $value ) ) : ?>
+  <?php if ( !empty( $options['data'] ) ) : ?>
 
-    <?php foreach ($value as $key => $val) : ?>
+    <?php foreach ($options['data'] as $key => $val) : ?>
       <?php
         // check to see if this is the chosen value
-        $active = $val['active'] === true ? ' active' : false;
-        if($val['active'] === true) { var_trace(var_export($val, true)); }
-        $tile_pod = pods('tile', $val['slug']);
+        $active = ($key === $active_tile_id) ? ' active' : false;
+        $tile_pod = pods('tile', $key);
 
-        $tile_title = $val['name'];
+        $tile_title = $tile_pod->field('name');
         $tile_layout = $tile_pod->field('tile_layout.name');
         $description = $tile_title . ' (' . $tile_layout . ')';
-        $tile_image_src = wp_get_attachment_image_src( $tile_pod->field('image.id'), array(32,32), false);
+        $tile_image_src = wp_get_attachment_image_src( $tile_pod->field('image.ID', TRUE), array(32,32), false);
       ?>
-      <option value="<?php echo $val['id']; ?>" class="option<?php echo $active ?>" data-description="<?php echo $description; ?>"<?php if($tile_image_src) { echo ' data-imagesrc="' . $tile_image_src[0] . '"'; }  if($active) { echo ' selected="selected"'; }?>><?php echo $description; ?></div>
+      <option value="<?php echo $key; ?>" class="option<?php echo $active ?>" data-description="<?php echo $description; ?>"<?php if($tile_image_src) { echo ' data-imagesrc="' . $tile_image_src[0] . '"'; }  if($active) { echo ' selected="selected"'; }?>><?php echo $description; ?></div>
     <?php endforeach; ?>
   <?php endif; ?>
 </select>
