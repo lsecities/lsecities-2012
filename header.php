7282cf15e62c6b3d6bf9c36a95ab3119b8df8430
andrea rota <a.rota@lse.ac.uk>
2013-10-09 10:48:59 +0100

    discontinuing Chrome Frame

--- header.php
+++ header.php
@@ -105,8 +105,7 @@ var usernoiseButton = {"text":"Feedback","style":"background-color: #ff0000; col
     <!--[if lt IE 9 ]>
       <p class='flash top chromeframe'>
         You are using an outdated browser.
-        <a href="http://browsehappy.com/">Upgrade your browser today</a> or
-        <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a>
+        <a href="http://browsehappy.com/">Upgrade your browser today</a>
         to better experience this site.
       </p>
     <![endif]-->
