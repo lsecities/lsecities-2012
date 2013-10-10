cdb6a070a4625615f5a44a9fb6dea28fd5d64a03
andrea rota <a.rota@lse.ac.uk>
2013-10-10 17:39:46 +0100

    removed background image altogether - doing it via html elements

--- includes/pods/pods/event-programme/pods-event-programme.php
+++ includes/pods/pods/event-programme/pods-event-programme.php
@@ -83,6 +83,8 @@ function process_session($session_slug, $special_fields_prefix, &$all_speakers) 
   /**
    * Recursively process subsessions. HSL.
    */
+  var_trace($pod->field('sessions'), 'event programme - sessions');
+  
   if(count($pod->field('sessions')) > 0 ) {
     $session_fields = sort_linked_field($pod->field('sessions'), 'sequence', SORT_ASC);
     foreach($session_fields as $session_field) {
