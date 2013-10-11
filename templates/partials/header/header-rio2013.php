a5eb1353a7bf652f8ce9c5fbd513a0e6e8b61d45
andrea rota <a.rota@lse.ac.uk>
2013-10-11 21:59:00 +0100

    backported changes from feature branch

---
+++ templates/partials/header/header-rio2013.php
@@ -0,0 +1,18 @@
+    <img class="full-background-photo" src="http://files.lsecities.net/files/2013/05/IMAG0410_edited-1200x500-1024x426.jpg" />
+    <header id='header'>
+      <div>
+        <h1 id="rio2013title" class='row'><a href="/">City Transformations</a></h1>
+        <span class="twitter-hashtag"><a href="https://twitter.com/search?q=%40lsecities&f=realtime">@lsecities</a></span>
+        <div class='row' id='mainmenus'>
+          <nav class='twelvecol section-ancestor-<?php echo $lc_toplevel_ancestor ; ?>' id='level2nav'>
+            <ul>
+            <?php if($lc_toplevel_ancestor and $lc_level2nav): ?>
+              <?php echo $lc_level2nav ; ?>
+            <?php else: ?>
+              <li>&nbsp;</li>
+            <?php endif; ?>
+            </ul>
+          </nav>
+        </div><!-- #mainmenus -->
+      </div>
+    </header><!-- #header -->
