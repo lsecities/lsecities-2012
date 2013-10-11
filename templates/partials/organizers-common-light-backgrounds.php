05a182adfac078814e617c8922242b7780b5af2f
andrea rota <a.rota@lse.ac.uk>
2013-10-11 21:59:00 +0100

    modular organizer templates; consolidating partials location

---
+++ templates/partials/organizers-common-light-backgrounds.php
@@ -0,0 +1,10 @@
+  <li>
+    <a href="http://www.lse.ac.uk/" target="_blank">
+      <img alt="LSE" src="<?php bloginfo('stylesheet_directory'); ?>/images/lse_logo_blk.gif" />
+    </a>
+  </li>
+  <li>
+    <a href="http://www.alfred-herrhausen-gesellschaft.de/en/" target="_blank">
+      <img alt="Alfred Herrhausen Gesellschaft" src="<?php bloginfo('stylesheet_directory'); ?>/images/ahs_logo_blk.gif" />
+    </a>
+  </li>
