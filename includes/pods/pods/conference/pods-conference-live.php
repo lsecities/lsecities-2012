cd0083a59698a39954d5fd8f41bf88ae1b3a6ef0
andrea rota <a.rota@lse.ac.uk>
2013-10-17 16:26:56 +0100

    add twitter widget id if supplied

--- includes/pods/pods/conference/pods-conference-live.php
+++ includes/pods/pods/conference/pods-conference-live.php
@@ -11,7 +11,8 @@ function pods_prepare_conference_live($pod_slug) {
   $obj['live_streaming_video_embedcode'] = $pod->field('live_streaming_video_embedcode');
   $obj['live_streaming_notice'] = $pod->field('live_streaming_notice');
   $obj['live_twitter_querystring'] = $pod->field('live_twitter_querystring');
-
+  $obj['live_twitter_widget_id'] = $pod->field('live_twitter_widget_id');
+  
   $live_storify_stories_uris = preg_replace('/^https?:\/\//', '', explode("\n", $pod->field('live_storify_stories')));
 
   foreach($live_storify_stories_uris as $story_uri) {
