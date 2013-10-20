98d72199d0cb928f97ad748bc4420fd88dc3a136
andrea rota <a.rota@lse.ac.uk>
2013-10-20 22:04:58 +0100

    fade in background image

--- templates/microsites/rio2013/rio2013_footer.php
+++ templates/microsites/rio2013/rio2013_footer.php
@@ -2,6 +2,8 @@
   // set background image to one from a random pool
   var images = ['010', '020', '030', '040', '050', '060', '070', '080', '090', '100', '110', '120', '130', '140', '150', '160', '170', '180', '190', '200'];
   jQuery(function() {
-    jQuery('.full-background-photo').attr('src', '<?php echo lc_data('rio2013_background_image_prefix'); ?>' + images[Math.floor(Math.random() * images.length)] + '.jpg');
+    jQuery('.full-background-photo')
+      .attr('src', '<?php echo lc_data('rio2013_background_image_prefix'); ?>' + images[Math.floor(Math.random() * images.length)] + '.jpg')
+      .removeClass('loading');
   });
 </script>
