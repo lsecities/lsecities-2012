85d1432b59be349701e51fa8cdf6d427b60a5073
andrea rota <a.rota@lse.ac.uk>
2013-11-13 14:21:20 +0000

    added support for Theatrum Mundi-branded newsletters

---
+++ pages-newsletter-theatrum-mundi.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Newsletter - Theatrum Mundi
+ * Description: Template used for Theatrum Mundi Newsletters
+ * 
+ * @package LSECities2012
+ */
+define('WP_USE_THEMES', false);
+
+/**
+ * As this is a Theatrum Mundi-branded newsletter, set the branding
+ * query var.
+ */
+set_query_var('newsletter_branding', 'theatrum-mundi');
+
+require(get_stylesheet_directory() . '/includes/newsletter/pages-newsletter-common.php');
--- pages-newsletter.php
+++ pages-newsletter.php
@@ -5,27 +5,6 @@
  * 
  * @package LSECities2012
  */
-$TRACE_ENABLED = is_user_logged_in();
 define('WP_USE_THEMES', false);
 
-/**
- * Initialize data
- */
-$newsletter = pages_prepare_newsletter($post->ID);
-
-var_trace(var_export($newsletter, true));
-
-set_query_var('newsletter', $newsletter);
-
-/**
- * Dispatch to template based on whether the HTTP GET parameter
- * 'channel' is set to 'email' or not
- * Also dispatch to email template if no 'email' channel is specified but
- * page is not a quarterly newsletter (i.e. it doesn't have subsections)
- */
-if($_GET['channel'] === 'email' or (is_array($newsletter['sections']) and count($newsletter['sections']) == 0)) {
-  locate_template('templates/newsletter/newsletter-email.php', true, true);
-} else {
-  locate_template('templates/newsletter/newsletter-web.php', true, true);
-}
-?>
+require(get_stylesheet_directory() . '/includes/newsletter/pages-newsletter-common.php');
