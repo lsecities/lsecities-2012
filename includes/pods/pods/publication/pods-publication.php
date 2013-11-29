1c565f331808e682d14b648954513498d94ffdc5
andrea rota <a.rota@lse.ac.uk>
2013-11-29 12:46:28 +0000

    fetch value via field()

--- includes/pods/pods/publication/pods-publication.php
+++ includes/pods/pods/publication/pods-publication.php
@@ -19,41 +19,41 @@ function pods_prepare_publication($pod_slug) {
 function pods_prepare_table_of_contents($pod_slug) {
   // retrieve pod by slug
   $pod = pods('publication_wrappers', $pod_slug);
-  
+
   // return if no such pod was found
   if(!$pod->exists()) {
     return;
   }
-  
+
   // initialize output array
   $obj = array();
-  
+
   $obj['title'] = $pod->field('name');
-  
+
   if(count($pod->field('articles'))) {
     $sections = array();
     foreach(preg_split("/\n/", $pod->field('sections')) as $section_line) {
       preg_match("/^(\d+)?\s?(.*)$/", $section_line, $matches);
       array_push($sections, array( 'id' => $matches[1], 'title' => $matches[2]));
     }
-    
+
     if(!count($sections)) {
       $sections = array("010" => "");
     }
-    
+
     foreach($sections as $section) {
       $articles = array();
-      
+
       foreach($pod->field('articles', array('orderby' => 'sequence ASC')) as $article) {
-        
+
         $article_pod = pods('article', $article['id']);
         $article_lang2 = $article_pod->field('language');
-        
+
         var_trace(var_export($article_lang2, true), 'article_lang2');
-        
+
         if(preg_match("/^" . $section['id'] . "/", $article['sequence'])) {
           $this_article = array();
-          
+
           $this_article['uri'] = PODS_BASEURI_ARTICLES . '/' . $article['slug'] . '/en-gb/';
           $this_article['title'] = $article['name'];
           if(!empty($article_lang2['name'])) {
@@ -61,13 +61,13 @@ function pods_prepare_table_of_contents($pod_slug) {
             $this_article['lang2_title'] = $article['title_lang2'];
             $this_article['lang2_langname'] = $article_lang2['name'];
           }
-          
+
           $this_article['authors'] = array();
-          
+
           foreach(sort_linked_field($article_pod->field('authors'), 'family_name', SORT_ASC) as $author) {
             $this_article['authors'][] = $author['name'] . ' ' . $author['family_name'];
           }
-          
+
           /**
            * TODO:
            * add fields:
@@ -77,14 +77,14 @@ function pods_prepare_table_of_contents($pod_slug) {
             'title' => $this_article['title'],
             'uri' => $this_article['uri'],
             'authors' => $this_article['authors'],
-            'heading_image' => pods_image_url($article['heading_image'], 'original'),
+            'heading_image' => pods_image_url($article_pod->field('heading_image'), 'original'),
             'lang2_title' => $this_article['lang2_title'],
             'lang2_uri' => $this_article['lang2_uri'],
             'lang2_langname' => $this_article['lang2_langname']
           );
         }
       }
-      
+
       $obj['sections'][] = array(
         'id' => $section['id'],
         'title' => $section['title'],
@@ -92,6 +92,8 @@ function pods_prepare_table_of_contents($pod_slug) {
       );
     }
   }
-  
+
+	var_trace(var_export($obj, true), 'PUBLICATION_TOC');
+
   return $obj;
 }
