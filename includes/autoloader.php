a773f996f6aa60517ae431ff688501a76c154d8a
andrea rota <a.rota@lse.ac.uk>
2013-12-02 16:24:59 +0000

    moved Twig+MtHaml code to own file

---
+++ includes/autoloader.php
@@ -0,0 +1,9 @@
+<?php
+
+use MtHaml\Autoloader;
+use MtHaml\Environment;
+
+require_fragment('/vendor/autoload.php');
+
+Twig_Autoloader::register();
+Autoloader::register();
--- templates/newsletter/newsletter-web.php
+++ templates/newsletter/newsletter-web.php
@@ -1,22 +1,3 @@
 <?php
 
-use MtHaml\Autoloader;
-use MtHaml\Environment;
-
-require_once(get_stylesheet_directory() . '/vendor/autoload.php');
-
-Twig_Autoloader::register();
-Autoloader::register();
-
-$loader = new Twig_Loader_String();
-$twig = new Twig_Environment($loader);
-$twig->addExtension(new MtHaml\Support\Twig\Extension());
-
-$haml = new Environment('twig', array('enable_escaper' => false));
-
-$template = get_stylesheet_directory() . '/templates/newsletter/newsletter-web.php.haml';
-$compiled = $haml->compileString(file_get_contents($template), $template);
-
-$template_data = $newsletter;
-
-echo $twig->render($compiled, $template_data);
+echo LSECitiesWPTheme\Templating\render_template('templates/newsletter/newsletter-web.php.haml', $newsletter);
