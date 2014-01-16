b8ba9a09d97b699d48713bcfde19b1018058e173
andrea rota <a.rota@lse.ac.uk>
2014-01-16 16:20:07 +0000

    backported search page from dev to test page

--- assets/js/media-archive-app/js/controllers.js
+++ assets/js/media-archive-app/js/controllers.js
@@ -1,5 +1,21 @@
 var mediaArchiveApp = angular.module('mediaArchiveApp', []);
- 
+
+mediaArchiveApp.controller('StaticMediaArchiveCtrl', function ($scope, $http) {
+  $scope.items = [];
+  $scope.mediatypes = { audio: false, video: false};
+  
+  $scope.loadItems = function() {
+    var httpRequest = $http({
+      method: 'GET',
+      url: '/wp-content/themes/lsecities-2012/assets/js/media-archive-app/media_archive_data.json'
+    }).success(function(data, status) {
+      $scope.items = data.items;
+    });
+  };
+  
+  $scope.loadItems();
+});
+
 mediaArchiveApp.controller('MediaArchiveCtrl', function ($scope, $http) {
   $scope.items = [];
   $scope.mediatypes = { audio: false, video: false};
