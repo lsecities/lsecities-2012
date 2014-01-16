b20940893fe9998a0d22bc0b07b7a6d270acffcc
andrea rota <a.rota@lse.ac.uk>
2014-01-16 16:20:07 +0000

    backported search page from dev to test page

--- pods-media-archive-frontpage.php
+++ pods-media-archive-frontpage.php
@@ -1,17 +1,18 @@
 <?php
 /**
- * Template Name: Pods - Media Archive search
- * Description: Media Archive search webapp
+ * Template Name: Pods - Media Archive search (dev)
+ * Description: Media Archive search webapp (dev)
  *
  * @package LSECities2012
  */
 ?><?php
-$TRACE_ENABLED = is_user_logged_in();
 $TRACE_PREFIX = 'pods-media-archive-search';
 $pods_toplevel_ancestor = 306;
+wp_enqueue_script('angularjs', get_stylesheet_directory_uri() . '/assets/bower_components/angular/angular.min.js', '', '1.2.8', TRUE);
+wp_enqueue_script('media_archive_app', get_stylesheet_directory_uri() . '/assets/js/media-archive-app/js/controllers.js', '', '', TRUE);
 ?><?php get_header(); ?>
 
-<div role="main">
+<div role="main" data-ng-app="mediaArchiveApp">
   <?php if ( have_posts() ) : the_post(); endif; ?>
   <div id="post-<?php the_ID(); ?>" <?php post_class('lc-article lc-media-archive-search'); ?>>
     <div class='ninecol' id='contentarea'>
@@ -28,23 +29,83 @@ $pods_toplevel_ancestor = 306;
         </header>
         <?php endif; ?>
         
-        <article class='wireframe eightcol row'>
-          <header class='entry-header'>
-            <h1>Search media archive</h1>
-          </header>
-          <div class='entry-content article-text'>
-            <p>
-              <form action='' method='get'>
-                <input type='text' name='search' id='query' />
-                <input type='submit' value='Search' id='searchbutton' />
-              </form>
-            </p>
-            
-            <h2>Search results</h2>
-            <div id='searchresults'>
-            </div>
-          </div>
-        </article>
+        <article class="wireframe" data-ng-controller="StaticMediaArchiveCtrl">
+                <section class="clearfix queryarea">
+                  <header class="entry-header fourcol">
+                    <h1 class="entry-title article-title">Media archive</h1>
+                    <h2>Search</h2>
+                  </header>
+                  <div class="eightcol last">
+                    <form method="get" action="">
+                      <div class="fourcol">
+                        <h3>Format</h3>
+                        <ul>
+                          <li>
+                            <label>
+                              <input type="checkbox" value="lecture" disabled="disabled">lecture
+                            </label>
+                          </li>
+                          <li>
+                            <label>
+                              <input type="checkbox" value="talk" disabled="disabled">talk
+                            </label>
+                          </li>
+                          <li>
+                            <label>
+                              <input type="checkbox" value="conference-session" disabled="disabled">conference session
+                            </label>
+                          </li>
+                        </ul>
+                      </div>
+                      <div class="fourcol">
+                        <h3>Media</h3>
+                        <ul>
+                          <li>
+                            <label>
+                              <input type="checkbox" value="audio" checked="checked" disabled="disabled" data-ng-model="mediatypes.audio">audio
+                            </label>
+                          </li>
+                          <li>
+                            <label>
+                              <input type="checkbox" value="video" checked="checked" disabled="disabled" data-ng-model="mediatypes.video">video
+                            </label>
+                          </li>
+                        </ul>
+                      </div>
+                      <input data-ng-model="query" type="text" placeholder="free text search: enter keywords/speaker names here" name="search" id="query" class="tencol last" value="<?php echo((isset($_GET["search"])) ? htmlspecialchars($_GET["search"]) : ""); ?>">
+                    </form>
+                  </div>
+                </section>
+                <section class="clearfix">
+                  <div class="resultsarea">
+                    <h2>Search results</h2>
+                    <div id="searchresults"></div>
+                  </div>
+                </section>
+                <section class="ngapp">
+                  <ul>
+                    <li data-ng-repeat="item in items | filter:query">
+                      <h3>{{item.title}}</h3>
+                      <h4 data-ng-show="item.parent_sessions">Event session: <span data-ng-repeat="session in item.parent_sessions">{{session.name}}<span data-ng-show=" ! $last "> &raquo; </span></span></h4>
+                      <h4 data-ng-show="item.parent_event">Event: <a href="/media/objects/events/{{item.parent_event.slug}}">{{item.parent_event.name}}</a></h4>
+                      <div class="people">
+                        <span data-ng-show="item.session_speakers">
+                          <ul class="run-in comma-separated">
+                            Speakers: 
+                            <li data-ng-repeat="speaker in item.session_speakers">{{speaker.name}} {{speaker.family_name}}</li>
+                          </ul>
+                        </span>
+                      </div>
+                      <div class="media">
+                        <span data-ng-show="item.youtube_uri"><a href="http://youtu.be/{{item.youtube_uri}}">Watch</a></span>
+                        <span data-ng-show="item.audio_uri"><a href="{{item.audio_uri}}">Listen</a></span>
+                      </div>
+                    </li>
+                  </ul>
+                </section>
+              </article>
+              
+              
         <aside class='wireframe fourcol last entry-meta' id='keyfacts'>
           &nbsp;
         </aside><!-- #keyfacts -->
@@ -53,65 +114,6 @@ $pods_toplevel_ancestor = 306;
       </div><!-- .extra-content -->
     </div><!-- #contentarea -->
   </div><!-- #post-<?php the_ID(); ?> -->
-<script src="https://raw.github.com/janl/mustache.js/master/mustache.js"></script>
-<script type="text/javascript">
-  // tests: http://jsfiddle.net/xswVa/1/
-  $ = jQuery;
-  
-  var mTemplate = '<ul>\
-  {{#items}}\
-    <li><a href="https://youtu.be/{{youtube_uri}}">{{title}}</a></li>\
-  {{/items}}\
-  </ul>\
-  {{^items}}\
-    <p>No media items match your query.</p>\
-  {{/items}}';
-  
-  function runMediaQuery() {
-    var query = $('#query').val();
-
-    if(query.length) {
-      var datastring = 'search=' + query;
-      
-      $.ajax(
-        {
-          type: "GET",
-          url: "/media/search",
-          data: datastring,
-          dataType: "json",
-          cache: false,
-          success: function(content, status) {
-        <?php if(is_user_logged_in()): ?>
-            console.log('ajax status: ' + status + "\nsearch results: " + content);
-        <?php endif; ?>
-            $('#searchresults').html(Mustache.render(mTemplate, content));
-          }
-        }
-      );
-    } else {
-      $('#searchresults').html(Mustache.render(mTemplate, ''));
-    }
-  }
-  
-  var typewatch = (function(){
-    var timer = 0;
-    return function(callback, ms){
-      clearTimeout (timer);
-      timer = setTimeout(callback, ms);
-    }  
-  })();
-
-  jQuery(document).ready(function($) {
-    $('#searchresults').html(Mustache.render(mTemplate, ''));
-    $('#searchbutton').click(function(e) {
-      e.preventDefault();
-      runMediaQuery();
-    });
-    $('#query').keyup(function() {
-      typewatch(runMediaQuery, 500);
-    });
-  });
-</script>
 </div><!-- role='main'.row -->
 
 <?php get_sidebar(); ?>
