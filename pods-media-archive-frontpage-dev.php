35a2081ca4cc9f1d9a04e272c1904d89080e2424
andrea rota <a.rota@lse.ac.uk>
2014-01-16 15:01:28 +0000

    pre-populate search field from GET parameter

--- pods-media-archive-frontpage-dev.php
+++ pods-media-archive-frontpage-dev.php
@@ -72,7 +72,7 @@ wp_enqueue_script('media_archive_app', get_stylesheet_directory_uri() . '/assets
                           </li>
                         </ul>
                       </div>
-                      <input ng-model="query" type="text" placeholder="free text search: enter keywords/speaker names here" name="search" id="query" class="tencol last">
+                      <input ng-model="query" type="text" placeholder="free text search: enter keywords/speaker names here" name="search" id="query" class="tencol last" value="<?php echo((isset($_GET["search"])) ? htmlspecialchars($_GET["search"]) : ""); ?>">
                     </form>
                   </div>
                 </section>
