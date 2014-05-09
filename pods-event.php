adb852102ef28022df635a8fbc0db1d49be08cf3
andrea rota <a.rota@lse.ac.uk>
2014-05-09 18:44:17 +0100

    added support for event Picasa gallery with optional credits

--- pods-event.php
+++ pods-event.php
@@ -54,6 +54,12 @@ $media_items_output_counter = 1;
                 <?php if($obj['event_contact_info'] and $obj['is_future_event']): ?>
                   <aside class="booking-and-access"><?php echo $obj['event_contact_info']; ?></aside>
                 <?php endif; ?>
+                <?php if($obj['picasa_gallery_id']) :
+                  galleria_shortcode(array('picasa_album' => $obj['picasa_gallery_id']));
+                  if($obj['photo_gallery_credits']) : ?>
+                  <p><?php echo $obj['photo_gallery_credits']; ?></p>
+                  <?php endif; ($obj['photo_gallery_credits']) ?>
+                <?php endif; // ($obj['picasa_gallery_id']) ?>
               </article>
               <aside class='wireframe fourcol last' id='keyfacts'>
                 <dl>
