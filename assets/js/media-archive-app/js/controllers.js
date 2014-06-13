d6481bd6f79b077e03e1be0d393675a8100ee705
andrea rota <a.rota@lse.ac.uk>
2014-06-13 13:49:01 +0100

    default to XHR from dumped JSON, use static=false GET parameter to use fresh data

--- assets/js/media-archive-app/js/controllers.js
+++ assets/js/media-archive-app/js/controllers.js
@@ -1,41 +1,35 @@
 var mediaArchiveApp = angular.module('mediaArchiveApp', []);
 
-mediaArchiveApp.controller('StaticMediaArchiveCtrl', function ($scope, $http) {
-  $scope.items = [];
-  $scope.mediatypes = { audio: false, video: false};
-  
-  $scope.loadItems = function() {
-    var httpRequest = $http({
-      method: 'GET',
-      url: '/wp-content/themes/lsecities-2012/assets/js/media-archive-app/media_archive_data.json'
-    }).success(function(data, status) {
-      $scope.audio_video_items = data.audio_video_items;
-      $scope.articles = data.articles;
-    });
-  };
-  
-  $scope.loadItems();
-});
-
 mediaArchiveApp.controller('MediaArchiveCtrl', function ($scope, $http) {
   $scope.items = [];
   $scope.mediatypes = { "audio" : true, "video" : true };
   $scope.talktypes = { "lecture" : true, "conference_session" : true };
-    
+  var use_static_data = $location.search('static');
+  
   $scope.loadItems = function() {
-    var avItemsRequest = $http({
-      method: 'GET',
-      url: '/search/audio_video_items'
-    }).success(function(data, status) {
-      $scope.audio_video_items = data.items;
-    });
-    
-    var articleItemsRequest = $http({
-      method: 'GET',
-      url: '/search/articles'
-    }).success(function(data, status) {
-      $scope.articles = data.items;
-    });
+    if(use_static_data === 'false') {
+      var avItemsRequest = $http({
+        method: 'GET',
+        url: '/search/audio_video_items'
+      }).success(function(data, status) {
+        $scope.audio_video_items = data.items;
+      });
+      
+      var articleItemsRequest = $http({
+        method: 'GET',
+        url: '/search/articles'
+      }).success(function(data, status) {
+        $scope.articles = data.items;
+      });
+    } else {
+      var httpRequest = $http({
+        method: 'GET',
+        url: '/wp-content/themes/lsecities-2012/assets/js/media-archive-app/media_archive_data.json'
+      }).success(function(data, status) {
+        $scope.audio_video_items = data.audio_video_items;
+        $scope.articles = data.articles;
+      });
+    };
   };
   
   $scope.loadItems();
