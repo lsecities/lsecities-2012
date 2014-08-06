04ddf153357ff7e188cb4a36944006264e930d39
andrea rota <a.rota@lse.ac.uk>
2014-08-06 15:19:48 +0100

    add option to treat linked items as objects

--- lib/pods-object.php
+++ lib/pods-object.php
@@ -9,12 +9,20 @@ class PodsObject {
    * table and return it as an array.
    * @param Object $pod The Pod object
    * @param string $field_name The related table's field name
-   * @return array The related table values
+   * @param string $class The related object's class, if any exists
+   * @return array The related table values, if no class is associated
+   *   with the related table, or objects, otherwise
    */
-  public function initialize_related_object($pod, $field_name) {
+  public function initialize_related_object($pod, $field_name, $class = NULL) {
     $related = $pod->field($field_name);
-
+    
     if(!empty($related) and is_array($related)) {
+      if($class) {
+        foreach($related as $item) {
+          $objects[] = new $class($item['id']);
+        }
+        return $objects;
+      }
       return $related;
     }
 
