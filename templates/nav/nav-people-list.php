cddec310e276173a123c52029067c36555beca4d
andrea rota <a.rota@lse.ac.uk>
2014-08-07 13:40:12 +0100

    fix markup, make nav title visible

--- templates/nav/nav-people-list.php
+++ templates/nav/nav-people-list.php
@@ -3,16 +3,12 @@ $TRACE_ENABLED = is_user_logged_in();
 $TRACE_PREFIX = 'nav.php -- ';
 if($people_list):
 ?>
-<?php if(is_user_logged_in()) : ?>
+<nav id="whoswho-side-toc">
 <dl class="in-page-navigation">
   <dt><?php echo get_the_title($post->ID); ?></dt>
   <dd>
-<?php endif; ?>
-    <nav id="whoswho-side-toc">
     <?php echo $people_list['summary']; ?>
-    </nav>
-<?php if(is_user_logged_in()) : ?>
   </dd>
 </dl>
-<?php endif; ?>
+</nav>
 <?php endif; ?>
