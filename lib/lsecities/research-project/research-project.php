74d2f4411ef50407d67d6376c8277a0b6ca0c933
andrea rota <a.rota@lse.ac.uk>
2014-08-07 16:31:50 +0100

    added empty ResearchProject class - just using a constant

---
+++ lib/lsecities/research-project/research-project.php
@@ -0,0 +1,10 @@
+<?php
+namespace LSECitiesWPTheme;
+
+// Exit if accessed directly
+if ( !defined('ABSPATH')) exit;
+
+class ResearchProject extends PodsObject {
+  const PODS_NAME = 'people_group';
+  const PODS_PAGES_BASE_PATH = '/objects/research-projects';
+}
