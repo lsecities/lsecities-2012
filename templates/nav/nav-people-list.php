82a9ba7a6eae41b5bf31f6945c34254aeafbef6f
andrea rota <a.rota@lse.ac.uk>
2014-08-07 16:37:17 +0100

    switch to MtHAML nav template only

--- templates/nav/nav-people-list.php
+++ templates/nav/nav-people-list.php
@@ -1,18 +1,4 @@
 <?php
-$TRACE_ENABLED = is_user_logged_in();
-$TRACE_PREFIX = 'nav.php -- ';
-if($people_list):
-?>
-<nav id="whoswho-side-toc">
-<dl class="in-page-navigation">
-  <dt><?php echo get_the_title($post->ID); ?></dt>
-  <dd>
-    <?php echo $people_list['summary']; ?>
-  </dd>
-</dl>
-</nav>
-<?php elseif(lc_data('page_data')):
-
-FoundootsWPTheme\Templating\foundoots_get_template_part('nav/_people-list', lc_data('page_data'));
-
-endif; ?>
+if(lc_data('page_data')) {
+  FoundootsWPTheme\Templating\foundoots_get_template_part('nav/_people-list', lc_data('page_data'));
+} ?>
