103c118ffb63f8e60be9ac296214b876fad0032e
andrea rota <a.rota@lse.ac.uk>
2014-08-08 11:20:38 +0100

    remove legacy code

--- lib/autoloader.php
+++ lib/autoloader.php
@@ -4,12 +4,6 @@
  */
 require_fragment('/vendor/autoload.php');
 
-use MtHaml\Autoloader;
-use MtHaml\Environment;
-
-Twig_Autoloader::register();
-Autoloader::register();
-
 // Set up class autoloader for our own classes
 // spl_autoload_register(function ($class) {
 //  include 'lsecities/' . $class . '/' . $class . '.php';
