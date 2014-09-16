a9343c9c497cba864f79008557f09884bf15bdac
andrea rota <a.rota@lse.ac.uk>
2014-09-16 14:11:23 +0100

    added new research output categories to list

--- includes/pods/pods/research-project/pods-research-project.php
+++ includes/pods/pods/research-project/pods-research-project.php
@@ -60,7 +60,7 @@ function pods_prepare_research_project($pod_slug) {
   }
 
   // hardcoded list of WP categories used to group research outputs linked to a research project
-  $obj['research_output_categories'] = array('book', 'journal-article', 'book-chapter', 'research-report', 'conference-newspaper', 'conference-proceedings', 'conference-report', 'report', 'blog-post', 'interview', 'magazine-article');
+  $obj['research_output_categories'] = array('book', 'journal-article', 'book-chapter', 'research-report', 'conference-newspaper', 'conference-proceedings', 'conference-report', 'report', 'blog-post', 'interview', 'magazine-article', 'essay', 'book-review');
 
   $obj['research_outputs'] = get_project_research_outputs($pod);
   
