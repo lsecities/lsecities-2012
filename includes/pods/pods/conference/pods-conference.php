d3bbce450f36cf850275a7b8d78119b909e92d9e
andrea rota <a.rota@lse.ac.uk>
2014-10-03 18:23:23 +0100

    use suitable size for partner logos

--- includes/pods/pods/conference/pods-conference.php
+++ includes/pods/pods/conference/pods-conference.php
@@ -138,9 +138,9 @@ function get_conference_partners($pod, $partner_field) {
 
     // MONKEYPATCH_BEGIN
     if($_GET["siteid"] == 'ec2012') {
-      $logo_uri = pods_image_url($organization_pod->field('logo_white_raster'), 'original');
+      $logo_uri = pods_image_url($organization_pod->field('logo_white_raster'), [ 600, 300 ]);
     } else {
-      $logo_uri = pods_image_url($organization_pod->field('logo'), 'original');
+      $logo_uri = pods_image_url($organization_pod->field('logo'), [ 600, 300 ]);
     }
     // MONKEYPATCH_END
 
