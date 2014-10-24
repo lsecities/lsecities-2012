cf6073c8078d6f0c298bec13d90ae3607dc305ed
andrea rota <a.rota@lse.ac.uk>
2014-10-24 11:48:07 +0100

    moved js code to own file; added code to manage background photos

--- javascripts/themes/urban-age-twentyfifteen/urban-age-twentyfifteen.js
+++ javascripts/themes/urban-age-twentyfifteen/urban-age-twentyfifteen.js
@@ -2,13 +2,23 @@
  * Urban Age TwentyFifteen microsite theme - JS companion
  */
 
-/**
- * Add/remove sticky class to #header depending on whether page is scrolled past
- * its top.
- * From: http://www.webdesignerdepot.com/2014/05/how-to-create-an-animated-sticky-header-with-css3-and-jquery/
- * ToDo: check on window load
- */
 jQuery(function($) {
+  var image,
+      caption;
+
+  /**
+   * Add/remove sticky class to #header depending on whether page is scrolled past
+   * its top.
+   * From: http://www.webdesignerdepot.com/2014/05/how-to-create-an-animated-sticky-header-with-css3-and-jquery/
+   */
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 1) {  
+      $('#header').addClass("sticky");
+    } else {
+      $('#header').removeClass("sticky");
+    }
+  });
+
   /**
    * check initial status (e.g. if page is scrolled, then reloaded
    * we need to set the sticky class without having to wait for a
@@ -17,12 +27,42 @@ jQuery(function($) {
   if($(this).scrollTop() > 1) {  
     $('#header').addClass("sticky");
   }
-  
-  $(window).scroll(function() {
-    if($(this).scrollTop() > 1) {  
-      $('#header').addClass("sticky");
-    } else {
-      $('#header').removeClass("sticky");
+
+  if(images) {
+    image = images[Math.floor(Math.random() * images.length)];
+    // Create photo caption; include attribution if available
+    caption = image.description || '';
+    if(image.attribution_name) {
+      // do not add separator if there is no image.description
+      if(caption.length > 0) caption += ' - ';
+      caption += 'Photo by ' + image.attribution_name + '.';
     }
-  });
+    // Add link to full size photo
+    if(caption.length > 0) caption += ' ';
+    caption += '(<a href="' + image.uri + '" target="_blank">View full size</a>)';
+
+    $('.full-background-photo')
+      .attr('src', image.uri)
+      .attr('title', image.title)
+      .removeClass('loading')
+      .addClass('just-loaded');
+
+    $('#background-photo-explorer .caption').html(caption);
+  }
+
+  setTimeout(function() {
+    $('.full-background-photo').removeClass('just-loaded');
+  }, 3000);
+
+  /**
+   * Show background photo details (author/description) on hover
+   * on background photo explorer button
+   */
+  $('#background-photo-explorer .trigger')
+    .click(function() {
+      $('.full-background-photo').toggleClass('foreground');
+      $(this).text(function(i, text){
+        return text === "Show background photo" ? "Hide background photo" : "Show background photo";
+      })
+    });
 });
