a97eb37d7ff2082183d18613b9a8cb8c84eb525b
andrea rota <a.rota@lse.ac.uk>
2014-10-31 14:26:46 +0000

    json_encode strings that go to JavaScript

--- templates/microsites/delhi2014/delhi2014_footer.php
+++ templates/microsites/delhi2014/delhi2014_footer.php
@@ -1,10 +1,10 @@
 <script>
   // set background image to one from a random pool
   var images = [ <?php foreach($lc_conference_obj['conference_data']['microsite_background_photos'] as $background_photo) {
-    echo "{ uri: '" . $background_photo['uri'] . "',
-            title: '" . $background_photo['title'] . "',
-            attribution_name: '" . $background_photo['attribution_name'] . "',
-            description: '" . $background_photo['description'] . "' },\n "; } ?> ];
+    echo "{ uri: " . json_encode($background_photo['uri']) . ",
+            title: " . json_encode($background_photo['title']) . ",
+            attribution_name: " . json_encode($background_photo['attribution_name']) . ",
+            description: " . json_encode($background_photo['description']) . " },\n "; } ?> ];
 </script>
 
 <div id="background-photo-explorer">
