b7eb020d62793403b27d6d2afd9d5338de870c10
andrea rota <a.rota@lse.ac.uk>
2014-11-08 16:28:20 +0000

    fixed colour of logos - with technical debt (see: #25)

--- templates/partials/organizers.php
+++ templates/partials/organizers.php
@@ -4,7 +4,7 @@
     if('ec2012' === lc_data('microsite_id')) {
       load_template(dirname(__FILE__) . '/organizers-common-dark-backgrounds.php', TRUE);
       load_template(dirname(__FILE__) . '/organizers-ec2012.php', TRUE);
-    } elseif('rio2013' === lc_data('microsite_id')) {
+    } elseif('rio2013' === lc_data('microsite_id') or 'delhi2014' === lc_data('microsite_id')) {
       load_template(dirname(__FILE__) . '/organizers-common-light-backgrounds.php', TRUE);
     } else {
       load_template(dirname(__FILE__) . '/organizers-common-dark-backgrounds.php', TRUE);
