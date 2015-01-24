b3817cc65a824e31f24dd101db19ee12165f6d4c
andrea rota <a.rota@lse.ac.uk>
2015-01-24 16:22:43 +0000

    code formatting

--- pods-course.php
+++ pods-course.php
@@ -21,7 +21,8 @@ namespace LSECitiesWPTheme;
     'post_title' => get_the_title(),
     'course' => new Course(get_post_meta($post->ID, 'pod_slug', true))
   ]);
-  \SemanticWP\Templating::get_template_part('lsecities/_course', lc_data('page_data')); ?>
+  \SemanticWP\Templating::get_template_part('lsecities/_course', lc_data('page_data'));
+  ?>
   
   <?php get_template_part('templates/partials/socialmedia-share'); ?>
 
