2caadecbb52fe25d7d72c98265044177eea8121e
andrea rota <a.rota@lse.ac.uk>
2015-01-25 14:59:01 +0000

    allow uri_var_position to be provided; set default if none is provided

--- lib/theme-functions.php
+++ lib/theme-functions.php
@@ -6,13 +6,18 @@
  * @return String The permalink, if found, or null if not
  */
 function get_pod_permalink($config) {
+  // set defaults
+  if($config['from_uri'] === TRUE and empty($config['uri_var_position'])) {
+    $config['uri_var_position'] = 'last';
+  }
+  
   // If callee wants to retrieve permalink from post meta, try to do so
   if($config['from_post_meta'] and $config['post_id'] and $permalink = get_post_meta($config['post_id'], 'pod_slug', TRUE)) {
     return $permalink;
   }
   
   // If callee wants to retrieve permalink from URI, try to do so
-  if($config['from_uri'] and $permalink = pods_v('last', 'url')) {
+  if($config['from_uri'] and $permalink = pods_v($config['uri_var_position'], 'url')) {
     return $permalink;
   }
 }
