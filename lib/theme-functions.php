c5964bf873d324407a3c65e85dcad2eb601cc3e9
andrea rota <a.rota@lse.ac.uk>
2015-02-05 00:02:47 +0000

    new function to filter arrays of associative arrays

--- lib/theme-functions.php
+++ lib/theme-functions.php
@@ -23,3 +23,26 @@ function get_pod_permalink($config) {
     return $permalink;
   }
 }
+
+/**
+ * Given an array of associative arrays such as people as returned by
+ * $pod->field('authors') or so, strip out array elements from each item
+ * in the array, except the fields to keep as listed in $keys_to_keep.
+ * 
+ * @param Array $array The source array of associative arrays
+ * @param Array $keys_to_keep A list of keys of elements to keep; all
+ *   the items with other keys will be removed
+ * @return Array The result array with unwanted keys stripped off each
+ *   array element
+ */
+function filter_items($array, $keys_to_keep) {
+  if(!is_array($array)) {
+    return NULL;
+  }
+  
+  return array_map(function($item) use ($keys_to_keep) {
+    // see: http://stackoverflow.com/questions/4260086/php-how-to-use-array-filter-to-filter-array-keys
+    return array_intersect_key($item, array_flip($keys_to_keep));
+  },
+  $array);
+}
