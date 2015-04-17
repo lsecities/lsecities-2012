7f569d5b05f61bf70edfe5e49b12f24fff20f132
andrea rota <a.rota@lse.ac.uk>
2015-04-17 16:09:10 +0100

    restructure footer to include social media icons and signup form (closes: #75)

--- footer.php
+++ footer.php
@@ -16,8 +16,7 @@
 	<footer id="footer">
     <nav id="footerSitemap">
       <div class="row">
-        <div class="onecol">&nbsp;</div>
-        <div class="twocol">
+        <div class="threecol">
           <h1>About</h1>
           <ul>
             <?php wp_list_pages('title_li=&depth=1&child_of=617&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
@@ -27,13 +26,13 @@
             <?php wp_list_pages('title_li=&depth=1&child_of=421&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
           </ul>
         </div>
-        <div class="twocol">
+        <div class="threecol">
           <h1>Research</h1>
           <ul>
             <?php wp_list_pages('title_li=&depth=2&child_of=306&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
           </ul>
         </div>
-        <div class="twocol">
+        <div class="threecol">
           <h1>Publications</h1>
           <ul>
             <?php wp_list_pages('title_li=&depth=1&child_of=309&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
@@ -43,7 +42,7 @@
             <?php wp_list_pages('title_li=&depth=1&child_of=311&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
           </ul>
         </div>
-        <div class="twocol">
+        <div class="threecol last">
           <h1>Urban Age</h1>
           <ul>
             <?php wp_list_pages('title_li=&depth=1&child_of=94&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
@@ -55,9 +54,55 @@
           </ul>
           <?php endif; // (lc_data('microsite_id') === 'ec2012' or lc_data('microsite_id') === 'rio2013') ?>
         </div>
+      </div>
+      <div class="row">
+        <div class="threecol">
+          <h1>Subscribe to our updates</h1>
+          <!-- Begin MailChimp Signup Form -->
+          <div id="mc_embed_signup">
+          <form action="//lsecities.us4.list-manage.com/subscribe/post?u=6a19b1b794ce991fff919b68d&amp;id=1f3b65491d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
+              <div id="mc_embed_signup_scroll">
+            <div class="mc-field-group">
+              <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+            </label>
+              <input value="" name="EMAIL" class="required email" id="mce-EMAIL" type="email">
+            </div>
+            <div class="mc-field-group">
+              <label for="mce-FNAME">First Name  <span class="asterisk">*</span>
+            </label>
+              <input value="" name="FNAME" class="required" id="mce-FNAME" type="text">
+            </div>
+            <div class="mc-field-group">
+              <label for="mce-LNAME">Last Name  <span class="asterisk">*</span>
+            </label>
+              <input value="" name="LNAME" class="required" id="mce-LNAME" type="text">
+            </div>
+            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+            <div id="mce-responses" class="clear">
+              <div class="response" id="mce-error-response" style="display:none"></div>
+              <div class="response" id="mce-success-response" style="display:none"></div>
+            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;"><input name="b_6a19b1b794ce991fff919b68d_1f3b65491d" tabindex="-1" value="" type="text"></div>
+              <div class="clear"><input value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" type="submit"></div>
+            </div>
+          </form>
+          </div>
+          <!--End mc_embed_signup-->
+        </div>
+        <div class="threecol">
+          <h1>Keep in touch</h1>
+          <ul>
+            <li><a href="https://twitter.com/#!/LSECities">Twitter</a></li>
+            <li><a href="https://facebook.com/lsecities">Facebook</a></li>
+            <li><a href="https://youtube.com/urbanage">Youtube</a></li>
+            <li><a href="http://lsecities.net/feed/">News feed</a></li>
+          </ul>
+        </div>
+        <div class="threecol">
+          <?php get_template_part('templates/partials/colophon'); ?>
+        </div>
         <div class="twocol">
           <?php get_template_part('templates/partials/organizers'); ?>
-          <?php get_template_part('templates/partials/colophon'); ?>
         </div>
         <div class="onecol last">&nbsp;</div>
       </div>
