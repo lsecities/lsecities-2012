12117b9010c40226accc8655539bd36bcfb10298
andrea rota <a.rota@lse.ac.uk>
2015-09-03 20:26:19 +0100

    add class name

--- pods-pod-index.php
+++ pods-pod-index.php
@@ -55,7 +55,7 @@ get_header();
 
 <?php if ( have_posts() ) : the_post(); endif; ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('lc-article lc-index lc-index-for-pod-' . $pod_index_configuration['pod']); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('lc-article lc-index layout-lists-by-column lc-index-for-pod-' . $pod_index_configuration['pod']); ?>>
   <?php \SemanticWP\Templating::get_template_part($pod_index_configuration['template'], [ 'title' => $pod_index_configuration['title'], 'sections' => $objs ]); ?>
 </article>
 
