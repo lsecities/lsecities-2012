5bceaec784f474f0207f0145aed2c0d02f3eb004
andrea rota <a.rota@lse.ac.uk>
2015-10-06 12:59:19 +0100

    include class file

--- functions.php
+++ functions.php
@@ -48,6 +48,7 @@ require_fragment('/includes/pods/ui/lsecities-pods-ui.php' );
 require_fragment('/lib/theme-functions.php');
 require_fragment('/lib/pods-object.php');
 require_fragment('/lib/lsecities/article/article.php');
+require_fragment('/lib/lsecities/award/award.php');
 require_fragment('/lib/lsecities/city/city.php');
 require_fragment('/lib/lsecities/country/country.php');
 require_fragment('/lib/lsecities/course/course.php');
