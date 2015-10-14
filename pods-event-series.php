9b31f5f5f37626df1cefcc874d520379a59bda06
andrea rota <a.rota@lse.ac.uk>
2015-10-14 19:56:12 +0100

    initial WP template for event series

---
+++ pods-event-series.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Template Name: Pods - Event series
+ * Description: The template used for Event series Pods
+ *
+ * @package LSECities2012
+ */
+namespace LSECitiesWPTheme;
+
+?><?php get_header(); 
+/**
+ * Pods initialization
+ */
+
+// get permalink of pod via pod_slug meta field
+$pod_id = get_post_meta($post->ID, 'pod_slug', true);
+// if none is set, template may not be triggered via a WP page but through
+// a Pods page: try last fragment of URI
+if(empty($pod_id)) {
+  $pod_id = pods_v('last', 'url');
+}
+
+$event_series = new EventSeries($pod_id);
+$obj = $event_series->to_var();
+
+echo '<!-- event_series: ' . $pod_id . ' - ' . var_export($obj, TRUE) . ' -->';
+
+/**
+ * Copy gallery object to own variable for compatibility with
+ * gallery partial.
+ */
+$gallery = $obj['gallery'];
+
+?>
+
+<div role="main">
+
+<?php if ( have_posts() ) : the_post(); endif; ?>
+
+<div id="post-<?php the_ID(); ?>" <?php post_class('lc-article lc-conference-frontpage lc-event-series-frontpage'); ?>>
+
+  <?php \SemanticWP\Templating::get_template_part('lsecities/_conference', $obj); ?>
+
+          <?php get_template_part('nav'); ?>
+
+</div><!-- #post-<?php the_ID(); ?> -->
+</div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
