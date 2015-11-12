a371dc6131a3737d46abc7e4b4427f59e9fffda3
andrea rota <a.rota@lse.ac.uk>
2015-11-12 20:49:22 +0000

    full ua10 banner template

--- templates/partials/ua10-banner.php
+++ templates/partials/ua10-banner.php
@@ -1,8 +1,32 @@
 <div class="ua10-debates-banner">
   <div class="wrapper">
-    <div class="text">
-      <?php get_template_part('images/logos/ua10', 'logo_urbanage_2015.svg'); ?>
-      <p>Explore our new database of 10 years of Urban Age research and conferences. Since 2015, over ten conferences have been held in rapidly urbanising regions in Africa and Asia, as well as in mature urban regions in the Americas and Europe. As an event, the Urban Age catalyses the exchange of information, experiences and data across a global network of cities.</p>
+    <div class="heading">
+      <div class="text">
+        <?php echo file_get_contents(get_stylesheet_directory() . '/images/logos/ua10/logo-urbanage.svg'); ?>
+      </div>
+      <div class="action">
+        <div><span class="dismiss-banner">X</span></div>
+      </div>
+    </div>
+    
+    <div class="content">
+      <div class="text">
+        <p>Explore our new database of 10 years of Urban Age research and conferences. Since 2015, over ten conferences have been held in rapidly urbanising regions in Africa and Asia, as well as in mature urban regions in the Americas and Europe. As an event, the Urban Age catalyses the exchange of information, experiences and data across a global network of cities.</p>
+      </div>
+      <div class="action">
+        <div class="cta"><a href="https://urbanage.lsecities.net">Experience Urban Age <i class="icon-right-open"></i></a></div>
+        <div class="logos">
+          <a class="logo lsecities" href="https://lsecities.net/">
+          <?php echo file_get_contents(get_stylesheet_directory() . '/images/logos/ua10/logo-lsecities.svg'); ?>
+          </a>
+          <a class="logo lse" href="https://lse.ac.uk/">
+          <?php echo file_get_contents(get_stylesheet_directory() . '/images/logos/ua10/logo-lse.svg'); ?>
+          </a>
+          <a class="logo ahs" href="https://www.alfred-herrhausen-gesellschaft.de/index_en.htm">
+          <?php echo file_get_contents(get_stylesheet_directory() . '/images/logos/ua10/logo-ahs.svg'); ?>
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </div>
