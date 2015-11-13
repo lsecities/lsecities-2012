c05a797aade2f010bbbfa0dcc75388522c5d40d9
andrea rota <a.rota@lse.ac.uk>
2015-11-13 23:12:47 +0000

    update copy

--- templates/partials/ua10-banner.php
+++ templates/partials/ua10-banner.php
@@ -11,10 +11,10 @@
     
     <div class="content">
       <div class="text">
-        <p>Explore our new database of 10 years of Urban Age research and conferences. Since 2015, over ten conferences have been held in rapidly urbanising regions in Africa and Asia, as well as in mature urban regions in the Americas and Europe. As an event, the Urban Age catalyses the exchange of information, experiences and data across a global network of cities.</p>
+        <p>Our new Urban Age website will soon bring ten years of research of the Urban Age project to students, scholars and policy makers interested in cities.</p>
       </div>
       <div class="action">
-        <div class="cta"><a href="https://urbanage.lsecities.net">Experience Urban Age <i class="icon-right-open"></i></a></div>
+        <div class="cta"><a href="https://urbanage.lsecities.net/"><span>Experience Urban Age <span class="cta-arrow"><?php echo file_get_contents(get_stylesheet_directory() . '/images/icons/sb/arrow-right.svg'); ?></span></span></a></div>
         <div class="logos">
           <a class="logo lsecities" href="https://lsecities.net/">
           <?php echo file_get_contents(get_stylesheet_directory() . '/images/logos/ua10/logo-lsecities.svg'); ?>
