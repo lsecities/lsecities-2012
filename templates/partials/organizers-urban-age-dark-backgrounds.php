dc866ae1491833d65fab8ed926f01c98cce43c27
andrea rota <a.rota@lse.ac.uk>
2016-03-26 16:30:20 +0000

    use new AHG logo in UA section footer (closes: #153)

--- templates/partials/organizers-urban-age-dark-backgrounds.php
+++ templates/partials/organizers-urban-age-dark-backgrounds.php
@@ -1,5 +1,5 @@
   <li>
     <a href="http://www.alfred-herrhausen-gesellschaft.de/en/" target="_blank">
-      <img alt="Alfred Herrhausen Gesellschaft" src="<?php bloginfo('stylesheet_directory'); ?>/images/ahs_logo_white.gif" />
+      <img alt="Alfred Herrhausen Gesellschaft" src="<?php bloginfo('stylesheet_directory'); ?>/images/logos/ahg_logo_deutsch_white.png" />
     </a>
   </li>
