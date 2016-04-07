a56cb346811eed127a1c017d1f993f50b218de3d
andrea rota <a.rota@lse.ac.uk>
2016-04-07 22:18:17 +0100

    properly use MailChimp v3 API (closes: #155 - srsly)

--- includes/cf7-mailchimp.php
+++ includes/cf7-mailchimp.php
@@ -6,48 +6,59 @@
  * Mailchimp list
  */
 function cf7mailchimp_integration_on_submission($cf7_data) {
-  var_trace(var_export($cf7_data, TRUE), 'cf7_data');
+  // get CF7 submission data 
+  $submission = WPCF7_Submission::get_instance();
+  if(!$submission) {
+    return FALSE;
+  }
 
-  switch($cf7_data->id) {
-    case 6844:
+  $posted_data = $submission->get_posted_data();
+
+  // Calculate subscriber hash (MD5 of lowercase email address)
+  $subscriber_hash = md5(strtolower($posted_data['your-email']));
+
+  switch($cf7_data->id()) {
+    case '6844' :
       // the form being submitted is the Delhi 2014 register your interest one
       $MailChimp = new \DrewM\MailChimp\MailChimp(LSECITIES_MAILCHIMP_API_KEY);
       $list_id = '1f3b65491d';
-      $result = $MailChimp->post('lists/' . $list_id. '/members', [
+      $result = $MailChimp->put('lists/' . $list_id. '/members/' . $subscriber_hash, [
         'email_address' => $cf7_data->posted_data['your-email'],
+        'status' => 'subscribed',
         'merge_vars' => [
-          'FNAME' => $cf7_data->posted_data['first-name'],
-          'LNAME' => $cf7_data->posted_data['your-name'],
-          'ROLE' => $cf7_data->posted_data['role'],
-          'ORG' => $cf7_data->posted_data['organisation'],
-          'CITY' => $cf7_data->posted_data['city'],
-          'COUNTRY' => $cf7_data->posted_data['country'],
-          'TOPICS' => $cf7_data->posted_data['topics'],
-          '2014CONF' => $cf7_data->posted_data['interest-in-ua2014-conference'],
-          '2014UANEWS' => $cf7_data->posted_data['subscribe-to-all-updates'] ? 'All LSE Cities news' : 'Urban Age 2014 conference news only'
-        ],
-        'double_optin' => FALSE,
-        'update_existing' => TRUE,
+          'FNAME' => $posted_data['first-name'],
+          'LNAME' => $posted_data['your-name'],
+          'ROLE' => $posted_data['role'],
+          'ORG' => $posted_data['organisation'],
+          'CITY' => $posted_data['city'],
+          'COUNTRY' => $posted_data['country'],
+          'TOPICS' => $posted_data['topics'],
+          '2014CONF' => $posted_data['interest-in-ua2014-conference'],
+          '2014UANEWS' => $posted_data['subscribe-to-all-updates'] ? 'All LSE Cities news' : 'Urban Age 2014 conference news only'
+        ]
       ]);
       break;
 
-    case 11074:
+    case '11074':
       // the form being submitted is the Venice 2016 register your interest one
       $MailChimp = new \DrewM\MailChimp\MailChimp(URBANAGE_MAILCHIMP_API_KEY);
-      $list_id = '189097';
-      $result = $MailChimp->post('lists/' . $list_id. '/members', [
-        'email_address' => $cf7_data->posted_data['your-email'],
-        'merge_vars' => [
-          'FNAME' => $cf7_data->posted_data['first-name'],
-          'LNAME' => $cf7_data->posted_data['your-name'],
-        ],
-        'double_optin' => FALSE,
-        'update_existing' => TRUE,
+      $MailChimp->verify_ssl = FALSE;
+      $list_id = 'bba9d8e6ea';
+      $result = $MailChimp->put('lists/' . $list_id. '/members/' . $subscriber_hash, [
+        'email_address' => $posted_data['your-email'],
+        'status' => 'subscribed',
+        'merge_fields' => [
+          'FNAME' => $posted_data['first-name'],
+          'LNAME' => $posted_data['your-name'],
+        ]
       ]);
       break;
 
-    var_trace(var_export($result, TRUE), 'mailchimp API result');
   }
+
+  var_trace(var_export($result, TRUE), 'mailchimp API result');
+  var_trace($MailChimp->getLastError(), 'mailchimp_last_error');
+  var_trace($MailChimp->getLastResponse(), 'mailchimp_last_response');
 
   return $cf7_data;
 }
