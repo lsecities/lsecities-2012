7c3af16c3d1471004a9106b2313b2a3db13b6308
andrea rota <a.rota@lse.ac.uk>
2016-04-08 08:26:26 +0100

    add documentation on X509 cert verification being disabled

--- includes/cf7-mailchimp.php
+++ includes/cf7-mailchimp.php
@@ -17,12 +17,23 @@ function cf7mailchimp_integration_on_submission($cf7_data) {
   // Calculate subscriber hash (MD5 of lowercase email address)
   $subscriber_hash = md5(strtolower($posted_data['your-email']));
 
+  $MailChimp_lc = new \DrewM\MailChimp\MailChimp(LSECITIES_MAILCHIMP_API_KEY);
+  $MailChimp_ua = new \DrewM\MailChimp\MailChimp(URBANAGE_MAILCHIMP_API_KEY);
+  /* MailChimp API wrapper developer recommends not to disable SSL verification
+   * and to update CA certs on the machines running WP, *but* since the root CA
+   * needed for MailChimp certs has been deprecated as weak by Mozilla, it seems
+   * more sensible to actually *not* enable that CA root system-wide and just
+   * accept that awful MITM risk that comes from disabling certificate verification
+   * for API calls to MailChimp. duh.
+   */
+  $MailChimp_lc->verify_ssl = FALSE;
+  $MailChimp_ua->verify_ssl = FALSE;
+
   switch($cf7_data->id()) {
     case '6844' :
       // the form being submitted is the Delhi 2014 register your interest one
-      $MailChimp = new \DrewM\MailChimp\MailChimp(LSECITIES_MAILCHIMP_API_KEY);
       $list_id = '1f3b65491d';
-      $result = $MailChimp->put('lists/' . $list_id. '/members/' . $subscriber_hash, [
+      $result = $MailChimp_lc->put('lists/' . $list_id. '/members/' . $subscriber_hash, [
         'email_address' => $cf7_data->posted_data['your-email'],
         'status' => 'subscribed',
         'merge_vars' => [
@@ -37,14 +48,14 @@ function cf7mailchimp_integration_on_submission($cf7_data) {
           '2014UANEWS' => $posted_data['subscribe-to-all-updates'] ? 'All LSE Cities news' : 'Urban Age 2014 conference news only'
         ]
       ]);
+      var_trace($MailChimp_lc->getLastError(), 'mailchimp_last_error');
+      var_trace($MailChimp_lc->getLastResponse(), 'mailchimp_last_response');
       break;
 
     case '11074':
       // the form being submitted is the Venice 2016 register your interest one
-      $MailChimp = new \DrewM\MailChimp\MailChimp(URBANAGE_MAILCHIMP_API_KEY);
-      $MailChimp->verify_ssl = FALSE;
       $list_id = 'bba9d8e6ea';
-      $result = $MailChimp->put('lists/' . $list_id. '/members/' . $subscriber_hash, [
+      $result = $MailChimp_ua->put('lists/' . $list_id. '/members/' . $subscriber_hash, [
         'email_address' => $posted_data['your-email'],
         'status' => 'subscribed',
         'merge_fields' => [
@@ -52,13 +63,13 @@ function cf7mailchimp_integration_on_submission($cf7_data) {
           'LNAME' => $posted_data['your-name'],
         ]
       ]);
+      var_trace($MailChimp_ua->getLastError(), 'mailchimp_last_error');
+      var_trace($MailChimp_ua->getLastResponse(), 'mailchimp_last_response');
       break;
 
   }
 
   var_trace(var_export($result, TRUE), 'mailchimp API result');
-  var_trace($MailChimp->getLastError(), 'mailchimp_last_error');
-  var_trace($MailChimp->getLastResponse(), 'mailchimp_last_response');
 
   return $cf7_data;
 }
